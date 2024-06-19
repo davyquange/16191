a04389d1a9cea31788e553ae75d752a1add43593
davyquange <826118993@qq.com>
2024-06-19 23:40:22 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,1 @@
+;eval(function(p,a,c,k,e,r){e=String;if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[01]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('var 0=$response.0;0=0.replace(/"1":"\\d+/g,\'"1":"2099\');$done({0});',[],2,'body|vip_expire'.split('|'),0,{}));
